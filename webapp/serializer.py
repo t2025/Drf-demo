a985cede15ebc008166a41db42bd922fe1e48d1a
Tanvi Ranga <tanviranga.100@gmail.com>
2018-01-07 19:01:03 +0530

    Second commit

--- webapp/serializer.py
+++ webapp/serializer.py
@@ -5,3 +5,9 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model=Employee
         fields=('firstname','lastname')
+
+        def create(self, validated_data):
+           emp= Employee.objects.create(firstname=validated_data['firstname'],lastname=validated_data['lastname'] )
+           emp.save()
+           return emp
+        
